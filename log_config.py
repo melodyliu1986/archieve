a4a2ece9e89637ab593ae6db859281ff25d75256
soliu <soliu@redhat.com>
2015-12-24 13:34:35 +0800

    update files

--- log_config.py
+++ log_config.py
@@ -1,21 +1,40 @@
 import os
 import logging
+import logging.config
 
-error_path = os.getcwd() + "/test2.error"
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+def logging_simple_config():
+    error_path = os.getcwd() + "/test2.error"
 
-error_logger = logging.getLogger("error logger")
-error_logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-error_handler = logging.FileHandler(error_path)
-error_handler.setFormatter(formatter)
+    error_logger = logging.getLogger("error logger")
+    error_logger.setLevel(logging.INFO)
 
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
+    error_handler = logging.FileHandler(error_path)
+    error_handler.setFormatter(formatter)
 
-error_logger.addHandler(error_handler)
-error_logger.addHandler(stream_handler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
 
-error_logger.info("Info: test")
-error_logger.error("Error: test")
+    error_logger.addHandler(error_handler)
+    error_logger.addHandler(stream_handler)
+
+    error_logger.info("Info: test")
+    error_logger.error("Error: test")
+
+
+def logging_config_file(path_config_file):
+    logging.config.fileConfig(path_config_file)
+    root_logger = logging.getLogger("root")
+    root_logger.debug("Root: test!")
+    root_logger.info("Info: test!")
+
+    sku_logger = logging.getLogger("skuValidation")
+    sku_logger.debug("skuValidation: test!")
+    sku_logger.info("skuValidation: test")
+
+
+if __name__ == "__main__":
+    path_config = os.getcwd() + "/logging.conf"
+    logging_config_file(path_config)
