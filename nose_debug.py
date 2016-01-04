ed7d588c04546ab6084ba4b216ca31626670b408
soliu <soliu@redhat.com>
2016-01-04 16:54:14 +0800

    update files

--- nose_debug.py
+++ nose_debug.py
@@ -5,22 +5,20 @@ import logging
 import logging.config
 import unittest
 
-log_config_file = "{0}/logging.conf".format(os.getcwd())
-logger = logging.getLogger(__name__)
-
 
 class ADD(unittest.TestCase):
 
     def setUp(self):
-        self.file_name = "{0}/nose_debug.log".format(os.getcwd())
-        logging.config.fileConfig(log_config_file, defaults={"logfilename":self.file_name})
+        log_config_file_path = "{0}/logging.conf".format(os.getcwd())
+        logging_file_name = "{0}/test.log".format(os.getcwd())
+        logging.config.fileConfig(log_config_file_path, defaults={'logfilename': logging_file_name})
+        self.logger = logging.getLogger("skuValidation")
 
     def test_add(self):
-        logging.info("Test add function!")
-        print "Test add function"
+        self.logger.info("Test add function!")
         self.assertEqual(1+1, 2, 'Failed to validate the add function!')
 
     def test_multiply(self):
-        logging.info("Test multiply function!")
+        self.logger.info("Test multiply function!")
         self.assertTrue(4*4==15, 'Failed to validate the multiply function!')
 
