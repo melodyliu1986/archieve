0ac0087cc2a8873c3e625d986af2851ab4cfbcda
root <root@dhcp-129-237.nay.redhat.com>
2016-04-15 15:25:04 +0800

    add a directory 'python_prepare'

---
+++ python_prepare/non_compiled_language/hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+print "Hello, world!"
+print "Hello, python!"
---
+++ python_prepare/non_compiled_language/test.s
@@ -0,0 +1,27 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello, world."
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	movl	$.LC0, %edi
+	call	puts
+	movl	$0, %edi
+	call	exit
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 5.1.1 20150618 (Red Hat 5.1.1-4)"
+	.section	.note.GNU-stack,"",@progbits
